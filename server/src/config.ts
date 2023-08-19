835e3adc3b9d3a98a991716db8cafd8247bbb548
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-08-20 01:22:53 +0300

    start server and connect to mongoDB

---
+++ server/src/config.ts
@@ -0,0 +1,41 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+import 'dotenv/config'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "./config.env") });
+
+interface ENV {
+    PORT: number | undefined;
+    MONGO_URI: string | undefined;
+}
+
+interface Config {
+    PORT: number;
+    MONGO_URI: string;
+}
+
+const getConfig = (): ENV => {
+    return {
+        PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
+        MONGO_URI: process.env.MONGO_URI
+    };
+};
+
+const getSanitzedConfig = (config: ENV): Config => {
+    for (const [key, value] of Object.entries(config)) {
+        if (value === undefined) {
+            throw new Error(`Missing key ${key} in config.env`);
+        }
+    }
+    return config as Config;
+};
+
+const config = getConfig();
+
+const sanitizedConfig = getSanitzedConfig(config);
+
+export default sanitizedConfig;
---
+++ server/src/index.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser';
+import compression from 'compression'
+import * as mongoose from 'mongoose'
+import cors from 'cors'
+import config from './config.js'
+import 'dotenv/config'
+
+const app = express()
+
+const PORT = config.PORT
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+async function start() {
+    try {
+        await mongoose.connect(config.MONGO_URI), {
+            useNewURLParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }, () => {
+            console.log('connect to database')
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Server error:', error.message);
+        } else {
+            console.error('An unknown error occurred:', error);
+        }
+        process.exit(1)
+    }
+}
+
+start()
+
+app.listen(PORT, () => console.log(`Listening server on port ${PORT}`))
