a3b430227b7aad2fd43bbf58395a14b41a2a5232
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-08-22 19:05:06 +0300

    pino-pratty disabled

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,16 +1,16 @@
 import {pino, Logger} from 'pino'
 import 'dotenv/config'
 
-const level = process.env.LogLevel
+// const level = process.env.LogLevel
 
 const logger: Logger = pino({
-    transport: {
-        target: 'pino-pretty',
-    },
-    level,
-    base: {
-        pid: false,
-    },
+    // transport: {
+    //     target: 'pino-pretty',
+    // },
+    // level,
+    // base: {
+    //     pid: false,
+    // },
 })
 
 export default logger
