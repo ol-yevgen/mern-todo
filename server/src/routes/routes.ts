37e6c7c94b51d573e3b83f9d229e290614490067
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-08-22 18:57:59 +0300

    connection test

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,7 +1,9 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 import tasks from './tasks.routes.js'
 import user from './user.routes.js'
 const router = Router()
+
+router.get('/', async (req: Request, res: Response) => res.status(200).json({ message: 'test check is OK' }))
 
 router.use(tasks)
 router.use(user)
