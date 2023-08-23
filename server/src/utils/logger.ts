1d7489ad807737da77c00e9742648f48bf96f4bd
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-08-23 14:12:21 +0300

    logger pterry

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,17 +1,18 @@
 import { pino, Logger  } from 'pino'
 import 'dotenv/config'
 
-const level = process.env.LOG_LEVEL
+// const level = process.env.LOG_LEVEL
+const logger: Logger = pino()
 
-const logger: Logger = pino({
-    transport: {
-        target: 'pino-pretty',
-    },
-    level,
-    base: {
-        pid: false,
-    },
-})
+// const logger: Logger = pino({
+//     transport: {
+//         target: 'pino-pretty',
+//     },
+//     level,
+//     base: {
+//         pid: false,
+//     },
+// })
 
 export default logger
 
