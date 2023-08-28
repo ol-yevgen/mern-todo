b0493f9d4fd725416e0d7e2f46fb89bde8d791b6
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-08-28 18:28:58 +0300

    done task code refactoring

--- server/src/controller/task.controller.ts
+++ server/src/controller/task.controller.ts
@@ -1,8 +1,7 @@
 import { NextFunction, Request, Response, RequestHandler } from 'express'
 import Task from '../models/task.model.js'
 import createHttpError from 'http-errors'
-import mongoose from 'mongoose'
-import logger from '../utils/logger.js'
+import mongoose from 'mongoose' 
 // import logger from '../utils/logger.js'
 
 export const getTasks: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
@@ -84,13 +83,16 @@ interface UpdateTaskParams {
 interface UpdateTaskBody {
     title?: string,
     text?: string
+    done: boolean 
 }
 
 export const updateTask: RequestHandler<UpdateTaskParams, unknown, UpdateTaskBody, unknown> = async (req, res, next) => {
     const taskId = req.params.taskId
-    const { title, text } = req.body
+    const { title, text, done } = req.body
     const titleExisted = await Task.findOne({ title })
     const textExisted = await Task.findOne({ text })
+    // const doneExisted = await Task.findByIdAndUpdate(taskId, {done: done})
+
 
     try {
         const task = await Task.findById(taskId).exec()
@@ -103,61 +105,72 @@ export const updateTask: RequestHandler<UpdateTaskParams, unknown, UpdateTaskBod
             throw createHttpError(400, 'Invalid task id')
         }
 
-        if (!title) {
-            throw createHttpError(400, 'Task must have a title')
+        if (task.id === done) {
+            
+            if (!title) {
+                throw createHttpError(400, 'Task must have a title')
+            }
+
+            if (!text) {
+                throw createHttpError(400, 'Task must have a text')
+            }
+
+            if (titleExisted) {
+                throw createHttpError(400, 'Task with same title already exist')
+            }
+
+            if (textExisted) {
+                throw createHttpError(400, 'Task with same text already exist')
+            }
+
+            task.title = title
+            task.text = text
+
+            const updatedTask = await task.save()
+
+            res.status(201).json(updatedTask)
+        } else {
+            const message = done ? 'Task has been done' : 'Task under the maintenance'
+            task.done = done
+
+            await task.save()
+
+            res.status(201).json(message)
         }
-
-        if (!text) {
-            throw createHttpError(400, 'Task must have a text')
-        }
-
-        if (titleExisted) {
-            throw createHttpError(400, 'Task with same title already exist')
-        }
-
-        if (textExisted) {
-            throw createHttpError(400, 'Task with same text already exist')
-        }
-
-        task.title = title
-        task.text = text
-
-        const updatedTask = await task.save()
-
-        res.status(201).json(updatedTask)
+        
     } catch (error) {
         next(error)
     }
 }
-interface DoneTaskBody {
-    done: boolean 
-}
+// interface DoneTaskBody {
+//     done: boolean 
+// }
 
-export const doneTask: RequestHandler<UpdateTaskParams, unknown, DoneTaskBody, unknown> = async (req, res, next) => {
-    const taskId = req.params.taskId
-    const { done } = req.body
+// export const doneTask: RequestHandler<UpdateTaskParams, unknown, DoneTaskBody, unknown> = async (req, res, next) => {
+//     const taskId = req.params.taskId
+//     const { done } = req.body
 
-    try {
-        const task = await Task.findById(taskId).exec()
-        const message = done ? 'Task has been done' : 'Task under the maintenance'
+//     try {
+//         const task = await Task.findById(taskId).exec()
+        
 
-        if (!task) {
-            throw createHttpError(404, 'Task not found')
-        }
+//         if (!task) {
+//             throw createHttpError(404, 'Task not found')
+//         }
 
-        if (!mongoose.isValidObjectId(taskId)) {
-            throw createHttpError(400, 'Invalid task id')
-        }
+//         if (!mongoose.isValidObjectId(taskId)) {
+//             throw createHttpError(400, 'Invalid task id')
+//         }
 
-        task.done = done
+//         task.done = done
 
-        await task.save()
+//         await task.save()
 
-        res.status(201).json(message)
-    } catch (error) {
-        next(error)
-    }
-}
+//         res.status(201).json(message)
+//     } catch (error) {
+//         next(error)
+//     }
+// }
 
 export const deleteTask: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     const taskId = req.params.taskId
--- server/src/routes/tasks.routes.ts
+++ server/src/routes/tasks.routes.ts
@@ -1,12 +1,11 @@
-import { getTasks, getTask, createTask,  doneTask, deleteTask } from '../controller/task.controller.js'
+import { getTasks, getTask, createTask, updateTask, deleteTask } from '../controller/task.controller.js'
 import { Router } from 'express'
 
 const router = Router()
 router.get('/api/tasks', getTasks)
 router.get('/api/tasks/:taskId', getTask)
 router.post('/api/tasks', createTask)
-// router.patch('/api/tasks/:taskId', updateTask)
-router.patch('/api/tasks/:taskId', doneTask)
+router.patch('/api/tasks/:taskId', updateTask)
 router.delete('/api/tasks/:taskId', deleteTask)
 
 export default router
