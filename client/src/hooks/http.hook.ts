e90d65b2a6bae7aebd97e97401f17ec07c94fc1d
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-09-02 06:40:22 +0300

    cors

--- client/src/hooks/http.hook.ts
+++ client/src/hooks/http.hook.ts
@@ -7,7 +7,7 @@ export const useHttp = () => {
 
     const BASE_URL = process.env.REACT_APP_BASE_URL as string;
 
-    const request = useCallback(async (url: string, _credentials: string = 'include', method: string = 'GET', body: any = null, headers: Record<string, string> = {}) => {
+    const request = useCallback(async (url: string, credentials: RequestCredentials = 'include', method: string = 'GET', mode: RequestMode = 'cors', body: any = null, headers: Record<string, string> = {}) => {
         setLoading(true);
 
         try {
@@ -17,7 +17,7 @@ export const useHttp = () => {
             }
             const TODO_API = BASE_URL + url
 
-            const response = await fetch(TODO_API, { method, body, headers })
+            const response = await fetch(TODO_API, { credentials, method, mode,body, headers })
             const data = await response.json()
             
             if (!response.ok) {
