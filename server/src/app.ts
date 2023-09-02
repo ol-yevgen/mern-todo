06653ca3780947c5e688ce393d2a436b9d83899f
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-09-02 06:24:55 +0300

    cors remove

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ import morgan from 'morgan';
 import 'dotenv/config'
 
 const PORT = process.env.PORT || 5050
-const BASE_FRONTEND_URL1 = process.env.BASE_FRONTEND_URL1 as string
+// const BASE_FRONTEND_URL1 = process.env.BASE_FRONTEND_URL1 as string
 // const BASE_FRONTEND_URL2 = process.env.BASE_FRONTEND_URL2 as string
 
 const app = express()
@@ -21,12 +21,12 @@ const app = express()
 //     origin: [BASE_FRONTEND_URL1, BASE_FRONTEND_URL2],
 //     credentials: true
 // }))
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', BASE_FRONTEND_URL1);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     res.setHeader('Access-Control-Allow-Origin', BASE_FRONTEND_URL1);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(express.json())
