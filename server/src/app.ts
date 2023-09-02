e251ab8ef6d2f5fbb787ce050ba3f3387e3be7c4
Yevgenii Oliinyk <oliinykyevgenii@gmail.com>
2023-09-02 06:21:24 +0300

    cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,21 +6,27 @@ import cookieParser from 'cookie-parser';
 import router from './routes/routes.js';
 import logger from './utils/logger.js';
 import morgan from 'morgan';
-import cors from 'cors'
+// import cors from 'cors'
 import 'dotenv/config'
 
 const PORT = process.env.PORT || 5050
 const BASE_FRONTEND_URL1 = process.env.BASE_FRONTEND_URL1 as string
-const BASE_FRONTEND_URL2 = process.env.BASE_FRONTEND_URL2 as string
+// const BASE_FRONTEND_URL2 = process.env.BASE_FRONTEND_URL2 as string
 
 const app = express()
 
-logger.info(BASE_FRONTEND_URL1)
+// logger.info(BASE_FRONTEND_URL1)
 
-app.use(cors({
-    origin: [BASE_FRONTEND_URL1, BASE_FRONTEND_URL2],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: [BASE_FRONTEND_URL1, BASE_FRONTEND_URL2],
+//     credentials: true
+// }))
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', BASE_FRONTEND_URL1);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(express.json())
